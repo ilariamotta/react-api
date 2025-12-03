412e4d40c725a24e13b67d4e7c3c44b712e0dd61
ilariamotta <ilaria.motta93@gmail.com>
2025-12-03 16:52:06 +0100

    feat: componente card

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./components/Card";
 
 export default function App() {
   const [attrici, setAttrici] = useState([]);
@@ -26,25 +27,8 @@ export default function App() {
       <div className="container text-center mt-4">
         <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
 {/* card */}
-{attrici.map((attrice) => (
-  <div key={attrice.id} className="col">
-    <div className="card mb-3 shadow-sm">
-      <div className="row g-0">
-        <div className="col-4">
-          <img src={attrice.image} alt={attrice.name} className="img-fluid rounded-start actor-img"/>
-        </div>
-        <div className="col-8">
-          <div className="card-body p-2 text-start">
-            <h5 className="card-title fw-bold mb-2">{attrice.name}</h5>
-            <p className="card-text m-0"><span className="fw-bold">Anno: {attrice.birth_year}</span></p>
-            <p className="card-text m-0"><span className="fw-bold">Nazionalità: </span>{attrice.nationality}</p>
-            <p className="card-text mt-2"><span className="fw-bold">Bio: </span>{attrice.biography}</p>
-<p className="card-text text-end"><small className="text-body-secondary"><span className="fw-bold">Premi: </span>{attrice.awards}</small></p>
-</div>
-        </div>
-      </div>
-    </div>
-  </div>
+{attrici.map((personaggio) => (
+<Card key={personaggio.id} personaggio={personaggio} />
 ))}
 {/* card */}
         </div>
---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+export default function Card({personaggio}) {
+    const {name, image, birth_year, nationality, biography, awards} = personaggio;
+
+    return (
+  <div className="col">
+    <div className="card mb-3 shadow-sm">
+      <div className="row g-0">
+        <div className="col-4">
+          <img src={image} alt={name} className="img-fluid rounded-start actor-img"/>
+        </div>
+        <div className="col-8">
+          <div className="card-body p-2 text-start">
+            <h5 className="card-title fw-bold mb-2">{name}</h5>
+            <p className="card-text m-0"><span className="fw-bold">Anno: {birth_year}</span></p>
+            <p className="card-text m-0"><span className="fw-bold">Nazionalità: </span>{nationality}</p>
+            <p className="card-text mt-2"><span className="fw-bold">Bio: </span>{biography}</p>
+<p className="card-text text-end"><small className="text-body-secondary"><span className="fw-bold">Premi: </span>{awards}</small></p>
+</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
