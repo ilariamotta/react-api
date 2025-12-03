4890fa3f21cfc888351c72a2c88bccd86f545eab
ilariamotta <ilaria.motta93@gmail.com>
2025-12-03 15:29:11 +0100

    feat: chiamata api e stampa in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [attrici, setAttrici] = useState([]);
 
+  useEffect(() => {
+    fetchAttrici();
+  }, []);
+
+  function fetchAttrici() {
+    axios
+      .get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((resp) => {
+        console.log(resp);
+      
+      });
+  }
+  
   return (
-    
-  )
+    <>
+      <Header />
+
+    </>
+  );
 }
 
-export default App
+
+
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+export default function Header() {
+    return(
+    <header className="bg-danger text-white-50 py-4"><h1 className="px-4">A superlative cast.</h1></header>
+    )
+}
